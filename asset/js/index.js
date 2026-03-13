bd116a2753d48663bf3b5420fe91cc3be772c9f7
williamchuang <@Zj199126h>
2026-03-13 14:40:32 +0800

    新增 RWD

---
+++ asset/js/index.js
@@ -0,0 +1,10 @@
+import { qa01 } from './QA_01.js';
+import { qa02 } from './QA_02.js';
+import { qa03 } from './QA_03.js';
+import { qa04 } from './QA_04.js';
+import { qa05 } from './QA_05.js';
+import { qa06 } from './QA_06.js';
+import { qa07 } from './QA_07.js';
+import { qa08 } from './QA_08.js';
+
+export default { qa01, qa02, qa03, qa04, qa05, qa06, qa07, qa08 };
