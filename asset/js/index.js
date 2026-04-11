652e6306027ffe448685f2c75b795b144f17fda5
William.Chuang <william.chuang@WilliamChuangs-MacBook-Pro.local>
2026-04-11 15:54:01 +0800

    fix - 修改MIS考試內容資料結構

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,10 +1,11 @@
-import { qa01 } from './QA_01.js';
-import { qa02 } from './QA_02.js';
-import { qa03 } from './QA_03.js';
-import { qa04 } from './QA_04.js';
-import { qa05 } from './QA_05.js';
-import { qa06 } from './QA_06.js';
-import { qa07 } from './QA_07.js';
-import { qa08 } from './QA_08.js';
+const keys = ['qa01', 'qa02', 'qa03', 'qa04', 'qa05', 'qa06', 'qa07', 'qa08'];
+const base = new URL('../data/', import.meta.url).href;
 
-export default { qa01, qa02, qa03, qa04, qa05, qa06, qa07, qa08 };
+const loadQuestions = async () => {
+  const results = await Promise.all(
+    keys.map(key => fetch(`${base}${key}.json`).then(r => r.json()))
+  );
+  return Object.fromEntries(keys.map((key, i) => [key, results[i]]));
+};
+
+export default loadQuestions;
